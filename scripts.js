e694f75cf4620e5c25aeac4113d50fda76e6d8cc
Jake Lauer <jake.lauer27@gmail.com>
2018-08-23 12:57:57 -0600

    Add Javascript photo scroller, remove CSS photo

--- scripts.js
+++ scripts.js
@@ -1,24 +1,45 @@
-var photos = document.querySelectorAll(".carter-photo");
+var photos = document.querySelectorAll(".slide");
+var photoIndicators = document.querySelectorAll(".slide-button")
 var rightArrow = document.querySelector(".arrow-right");
 var leftArrow = document.querySelector(".arrow-left");
 
-var slideIndex = 0;
+var index = 0;
 
-
+showPhoto();
 
 rightArrow.addEventListener("click", function(){
-
+  changeSlideIndex(1, "scrollInRight")
 });
 
 leftArrow.addEventListener("click", function(){
-
+  changeSlideIndex(-1, "scrollInLeft")
 });
 
 
+function changeSlideIndex(plusMinus, direction) {
+  index += plusMinus;
+  if (index < 0) {
+    index = photos.length - 1;
+  } 
+  if (index > (photos.length - 1)) {
+    index = 0;
+  }
+  showPhoto(direction);
+}
 
-
-photos[0].classList.add("photoDisplay")
-
-function showPhoto(n) {
-    photos[n].
+function showPhoto(scrollDirection) {
+  for (i = 0; i < photos.length; i++) {
+    photos[i].classList.add("noDisplay");
+    photoIndicators[i].classList.add("buttonNoHighlight");
+  }
+  photos[index].classList.remove("noDisplay");
+  photoIndicators[index].classList.remove("buttonNoHighlight");
+  photos[index].classList.add(scrollDirection);
+  rightArrow.disabled = true;
+  leftArrow.disabled = true;
+  setTimeout(function(){
+    rightArrow.disabled = false;
+    leftArrow.disabled = false;
+    photos[index].classList.remove(scrollDirection);
+}, 1000);
 }
