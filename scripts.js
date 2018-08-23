2025f21d8e2205e0409c4b2da013dc796d65d3d5
Jake Lauer <jake.lauer27@gmail.com>
2018-08-22 21:02:29 -0600

    Add basic slideshow functionality and slide buttons

---
+++ scripts.js
@@ -0,0 +1,24 @@
+var photos = document.querySelectorAll(".carter-photo");
+var rightArrow = document.querySelector(".arrow-right");
+var leftArrow = document.querySelector(".arrow-left");
+
+var slideIndex = 0;
+
+
+
+rightArrow.addEventListener("click", function(){
+
+});
+
+leftArrow.addEventListener("click", function(){
+
+});
+
+
+
+
+photos[0].classList.add("photoDisplay")
+
+function showPhoto(n) {
+    photos[n].
+}
